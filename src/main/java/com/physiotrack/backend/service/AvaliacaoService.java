dbc404164d83b3a0c0102471dcb34bd8abf3b1c0
Bruno Borelli <rodriguesborellibruno@gmail.com>
2025-06-17 00:57:19 -0300

    #7 - Ajuste avaliacao register e WIP Jasper report

--- src/main/java/com/physiotrack/backend/service/AvaliacaoService.java
+++ src/main/java/com/physiotrack/backend/service/AvaliacaoService.java
@@ -1,15 +1,32 @@
 package com.physiotrack.backend.service;
 
+import com.physiotrack.backend.exceptions.ObjectNotFoundException;
 import com.physiotrack.backend.model.atendimento.Atendimento;
 import com.physiotrack.backend.model.avaliacao.Avaliacao;
 import com.physiotrack.backend.model.avaliacao.AvaliacaoRequestDTO;
+import com.physiotrack.backend.model.avaliacao.AvaliacaoResponseDTO;
 import com.physiotrack.backend.model.user.User;
 import com.physiotrack.backend.repository.AvaliacaoRepository;
 import lombok.RequiredArgsConstructor;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -24,18 +41,55 @@ public class AvaliacaoService {
 
     private final ModelMapper mapper;
 
-    public Avaliacao register (AvaliacaoRequestDTO dto){
+    public Avaliacao register (AvaliacaoRequestDTO dto, Long atendimentoId){
         User user = userService.getLoggedUser();
-        Atendimento atendimento = atendimentoService.findLast(user.getId());
+        Atendimento atendimento = atendimentoService.obterAtendimento(atendimentoId);
         Avaliacao avaliacao = new Avaliacao();
         mapper.map(dto, avaliacao);
         avaliacao.setAtendimento(atendimento);
+        avaliacao.setData(LocalDateTime.now());
         insetAvaliacao(avaliacao);
         return avaliacao;
     }
 
     public void insetAvaliacao(Avaliacao avaliacao){
         avaliacaoRepository.save(avaliacao);
+    }
+
+    public Avaliacao findAvaliacaoById(Long id){
+        return avaliacaoRepository.findById(id)
+                .orElseThrow(()->  new ObjectNotFoundException("Avaliação não encontrada"));
+    }
+
+    public byte[] gerarPdf(Long id) throws JRException, IOException {
+        // Busca a avaliação
+        Avaliacao avaliacao = findAvaliacaoById(id);
+
+        // Carrega a imagem do classpath
+        InputStream imageStream = getClass().getResourceAsStream("/jasper/av_V_1/imgBg1.png");
+        BufferedImage image = null;
+        if(imageStream != null){
+          image = ImageIO.read(imageStream);
+        }
+
+        // Define os parâmetros
+        Map<String, Object> parametros = new HashMap<>();
+        parametros.put("bgImage", image);
+        parametros.put("nomePaciente", avaliacao.getAtendimento().getPaciente().getNome());
+        parametros.put("nomeFisioterapeuta", avaliacao.getAtendimento().getUsuario().getPessoa().getNome());
+        parametros.put("data", avaliacao.getAtendimento().getDataAtendimento().toString());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(List.of(avaliacao));
+        //
+        InputStream jrxml = getClass().getResourceAsStream("/jasper/av_V_1/AV_V_1.jrxml");
+        JasperReport report = JasperCompileManager.compileReport(jrxml);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(report, parametros, dataSource);
+//        // Carregar o template compilado
+//        InputStream jasperStream = getClass().getResourceAsStream("/jasper/av_V_1/AV_V_1.jasper");
+//        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperStream, parametros, dataSource);
+
+        // Exportar para PDF
+        byte[] pdf = JasperExportManager.exportReportToPdf(jasperPrint);
+        return pdf;
     }
 
 }
